a20dc17c696e8dd959f51d71bb0dac9341d9bbd4
Kirell Benzi <hello@kirelion.com>
2024-09-27 16:23:05 +0200

    clean up

--- server.py
+++ server.py
@@ -3,21 +3,6 @@ import argparse
 
 from model import Llama3
 
-
-class Llama3VisionAPI(ls.LitAPI):
-    def setup(self, device):
-        self.model = Llama3(device)
-
-    def decode_request(self, request):
-        return self.model.apply_chat_template(request.messages)
-
-    def predict(self, inputs, context):
-        yield self.model(inputs)
-
-    def encode_response(self, outputs):
-        for output in outputs:
-            yield {"role": "assistant", "content": self.model.decode_tokens(output)}
-
 class Llama3VisionAPI(ls.LitAPI):
     def setup(self, device):
         self.model = Llama3(device)
